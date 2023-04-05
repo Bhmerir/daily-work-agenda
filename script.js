84a571754590e067c16f6ff615140283c7e1bfa6
Bahareh <mer_ir@yahoo.com>
2023-04-04 22:32:40 -0700

    build dynamically the work hours with their save buttons and textareas

--- script.js
+++ script.js
@@ -2,6 +2,55 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  var containerEl = $(".container-lg")
+  var today = dayjs();
+  var currentHour = today.format("H")
+  for(var i = 9; i < 17; i++){
+    var parentdiv = $("<div>").addClass("row time-block");
+    parentdiv.attr("id", "hour-"+i);
+    if ((currentHour >= 0) && (currentHour < 9)) {
+      parentdiv.addClass("future");
+    } else if (currentHour >= 17) {
+      parentdiv.addClass("past");
+    }
+    else{
+      if (currentHour < i){
+        parentdiv.addClass("past");
+      }
+      else if (currentHour > i){
+        parentdiv.addClass("future");
+      }
+      else{ //current hour
+        parentdiv.addClass("present");
+      }
+    }
+    var nextdiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3");
+    var AMPM = "";
+    if (i < 12){
+      AMPM = "AM";
+      nextdiv.text(i + AMPM);
+    }
+    else if (i == 12){
+      nextdiv.text("12PM");
+    }
+    else
+    {
+      AMPM = "PM";
+      nextdiv.text((i - 12) + AMPM);
+    }
+
+    var textareaSec = $("<textarea>").addClass("col-8 col-md-10 description");
+    textareaSec.attr("rows", "3");
+    var saveBtn = $("<button>").addClass("btn saveBtn col-2 col-md-1");
+    saveBtn.attr("aria-label", "save");
+    var iSec = $("<i>").addClass("fas fa-save");
+    iSec.attr("aria-hidden", "true");
+    saveBtn.append(iSec);
+    parentdiv.append(nextdiv, textareaSec, saveBtn);
+    containerEl.append(parentdiv);
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
