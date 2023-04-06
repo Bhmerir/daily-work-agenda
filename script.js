bf8bd9ca7715d905ea560338c5038652ba1d1f4b
Bahareh <mer_ir@yahoo.com>
2023-04-05 22:11:27 -0700

    save the to-do list in local storage, and retrieve and show it once the page refreshes

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 first key is called hour and keep the value of hour that a program is written for that,
 second key is called program and its value is the written program for that hour */
 var toDoList = [];
+/* The time of start and end work hours have been defined globally to give the flexibility to change it easier 
+based on each company policy*/
+var startWorkHour = 9;
+var EndWorkHour = 17;
 
 $(function () {
-// building dynamically the elements and coloring them based on the time  
+//----------- building dynamically the elements and coloring them based on the time ---------------------- 
   var containerEl = $(".container-lg")
   var today = dayjs();
   var currentHour = today.format("H")
   var currentDay = today.format("dddd, MMMM D, YYYY")
   $("#currentDay").text(currentDay);
-  for(var i = 9; i < 17; i++){
+  for(var i = startWorkHour; i < EndWorkHour; i++){
     var parentdiv = $("<div>").addClass("row time-block");
     parentdiv.attr("id", "hour-"+i);
     if ((currentHour >= 0) && (currentHour < 9)) {
@@ -53,49 +57,76 @@ $(function () {
     saveBtn.append(iSec);
     parentdiv.append(nextdiv, textareaSec, saveBtn);
     containerEl.append(parentdiv);
-    //adding the saved to-do-list for each work hour to the related text-area 
+  }
+  //------------------------------------------------------------------------------------------------------
+
+  //--------- adding the saved to-do-list for each work hour to the related text-area --------------------
+  toDoList = [];
+  toDoList = JSON.parse(localStorage.getItem("toDoListStringify"));
+  if (!toDoList){
+    toDoList = [];
+  }
+  else{
+    showToDoList();
   }
 
+  //This function shows the saved program for each hour once page refreshed
+  function showToDoList(){
+    $.each(toDoList,function(index, value){
+      var toDoHour = toDoList[index].hour;
+      var chosenDivId = "#hour-" + toDoHour;
+      $(chosenDivId).children().eq(1).val(toDoList[index].program); //the textarea 
+    });
+  }
+  //-----------------------------------------------------------------------------------------------------
+
+  //------------------- saving the to do list of each hour after pressing save button -------------------
   $(".saveBtn").on("click", saveProgram)
-
-
+  /*This function makes an object for each hour and saves it to toDOList ,
+  and then saves the list in local storage*/
   function saveProgram(){
+    //making an object for that hour with its todo program
     var chosenDiv = $(this).parent()
     var chosenDivId = chosenDiv.attr("id");
     var chosenHourIdArray = chosenDivId.split("-");
     var chosenHour = chosenHourIdArray[1];
-    var program = chosenDiv.children().eq(1).val(); //.description
-    var programHour = {
-                        hour: chosenHour,
-                        program: program
-                      }
-    var notFound = true;
-    $.each(toDoList,function(index, value){
-      if (toDoList[index].hour == chosenHour){
-        toDoList[index].program = program;
-        notFound = false;
-      }
-    })
-    if(notFound){
-      toDoList.push(programHour)
-    } 
+    var program = chosenDiv.children().eq(1).val(); //the textarea 
+    //Only if a program is entered, it will be saved in local Storage after clicking save button, not empty ones
+    if(program.trim() !== ""){
+      var programHour = {
+                          hour: chosenHour,
+                          program: program.trim()
+                        }
+      
+      //load saved toDoList from local storage
+      var notFound = true;
+      /*search to-do list to see if a program has been set for that hour before or not 
+      if yes, it replaced the program of that hour,
+      If no, it adds the related programHour object to the array of toDoList*/
+      $.each(toDoList,function(index, value){
+        if (toDoList[index].hour == chosenHour){
+          toDoList[index].program = program.trim();
+          notFound = false;
+        }
+      });
+      if(notFound){
+        toDoList.push(programHour);
+      } 
+      
+    }
+    else{
+      //If user delete a saved program for an hour, that program will be deleted from toDoList and local storage
+      $.each(toDoList,function(index, value){
+        if(chosenHour == toDoList[index].hour)
+        {
+          toDoList.splice(index , 1);
+        }
+      });
+    }
+    //save our todolist in locol storage
+    localStorage.setItem("toDoListStringify", JSON.stringify(toDoList));
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //-----------------------------------------------------------------------------------------------------
 });
+
+
